9fe6115c03fc5ecee1df0e83d749726d20a06912
arefmaghdam <arefmaghdam@gmail.com>
2024-07-24 13:56:14 +0330

    add what is micro frontend

--- pages/components/git/what-is-micro-frontend.tsx
+++ pages/components/git/what-is-micro-frontend.tsx
@@ -10,7 +10,13 @@ const History = () => {
       <CodePreview
         imgURL=""
         content={`
-    // 
+    // micro frontends are where we take a monolithic application and we divide it into multiple smaller applications,
+    // each of these smaller applications are responsible for one distinct major feature of our product
+    // as much as possible we try to prevent these different micro applications from communicating with each other directly
+    // we make use of micro frontends because it allows multiple different engineering teams to work on the same overal application but in total isolation
+    // so if engineering team a or number one make some kind of breaking change to their app it's not going to necessarily break some other part of our application
+    // in addition when we start to divide our application out into micro frontends it makes each of these smaller parts a lot easierto understand 
+    // and make changes to without accidentally breaking some other part of our app as well
         `}
       />
     </div>
